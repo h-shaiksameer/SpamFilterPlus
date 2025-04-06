b1558172c15bb4bda3f3277f2a40c9bf401bb134
Sameerq7 <227r1a05q7@cmrtc.ac.in>
2025-04-06 19:35:58 +0530

    deployment

--- app.py
+++ app.py
@@ -246,4 +246,4 @@ def download_spam_csv():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
